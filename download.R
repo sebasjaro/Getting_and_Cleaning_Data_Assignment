78ebe3aa9b5fd9098f307ba33272871e799eca92
sebasjaro <64091585+sebasjaro@users.noreply.github.com>
2020-06-07 17:59:18 -0300

    download.R

---
+++ download.R
@@ -0,0 +1,6 @@
+if (!file.exists("data")) {
+        dir.create("data")
+}
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl, destfile = "./data/dataset.zip",mode ="wb")
+unzip("./data/dataset.zip")
